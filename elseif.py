093f8e5a2d03d7c7cbaeb31518f25687ef604238
sajidamullani <85106150+sajidamullani@users.noreply.github.com>
2021-08-05 09:53:21 +0530

    Add files via upload

---
+++ elseif.py
@@ -0,0 +1,20 @@
+
+x=5
+if  x>5:
+	print("x is gretter then 5")
+	print(x*2)
+elif x==5:
+	print("x is 5")
+else:
+	print("x is NOT greater than 5")
+
+y=150
+z=100
+if y==5 and type(y) is int:
+	print("y equals 5. y is an integer")
+	print(y)
+elif y==10 and type(y) is int:
+	print("y is an ineteger, but is not equal to 5")
+else:
+	print("z is 100")
+	
